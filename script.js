b4595eff2292a884f01fe53de53a79a06b1b3e51
erentaskiran <101654590+erentaskiran@users.noreply.github.com>
2022-11-12 00:08:24 +0300

    Main

--- script.js
+++ script.js
@@ -20,45 +20,24 @@ function random(){
     }
 }
 function tahminoyunu(){
-    
     random();
    tahmin=document.getElementById("giris").value; 
-   
     hak--;
     if(hak==0){
-        
         return document.getElementById("sonuc").innerHTML=("Bulamadınız.Sayı "+rnd+" idi");
-        
     }
     if (tahmin>rnd) {
-        
-        
         return document.getElementById("sonuc").innerHTML=("düşür");
-        
     }
     else if(tahmin==rnd){
-      
-        
         return document.getElementById("sonuc").innerHTML=("Tebrikler doğru bildiniz "),document.getElementById("puanlar").innerHTML=(puanhsp()+" puan aldınız");
-
-        
-        
     }
     else if (tahmin<rnd) {
-        
-
         return document.getElementById("sonuc").innerHTML=("yükselt");       
-        
     }
-    
-    
-    
 }
-
-
 function puanhsp(){
    
    let a= 100-((degismeyenhak-hak-1)*(100/degismeyenhak));
    return a;
 }
-
