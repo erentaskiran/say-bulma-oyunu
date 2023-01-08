5dbccd3b73e933885edc58492753956cf14f3c18
erentaskiran <101654590+erentaskiran@users.noreply.github.com>
2023-01-08 21:14:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,16 @@
-let tahmin;
-let rnd
-let hak;
-let enyuksek=0,endusuk=0;
-let degismeyenhak;
-function maxvalue(){
-    enyuksek=document.getElementById("maxvalue").value;
-}
-function minvalue(){
-    endusuk=document.getElementById("minvalue").value;
-}
-function hakekle(){
-    hak=document.getElementById("asd").value;
-    degismeyenhak=hak;
-}
-function random(){
-     rnd=Math.floor( Math.random()*enyuksek);
-    while(rnd<endusuk){
-        rnd=Math.floor( Math.random()*enyuksek);
+let rndsayi=0;
+let tahmin=0;
+
+function oyun(){
+    rndsayi= Math.floor( ((Math.random()*10)+1));
+    do {
+        tahmin=  prompt("1 ile 10 arasında bir sayı tahmin edin");
+    if(tahmin==rndsayi){
+        alert("kazandın");
     }
+    else{
+        alert("yanlış");
+    }
+    } while (tahmin!=rndsayi);
 }
-function tahminoyunu(){
-    random();
-   tahmin=document.getElementById("giris").value; 
-    hak--;
-    if(hak==0){
-        return document.getElementById("sonuc").innerHTML=("Bulamadınız.Sayı "+rnd+" idi");
-    }
-    if (tahmin>rnd) {
-        return document.getElementById("sonuc").innerHTML=("düşür");
-    }
-    else if(tahmin==rnd){
-        return document.getElementById("sonuc").innerHTML=("Tebrikler doğru bildiniz "),document.getElementById("puanlar").innerHTML=(puanhsp()+" puan aldınız");
-    }
-    else if (tahmin<rnd) {
-        return document.getElementById("sonuc").innerHTML=("yükselt");       
-    }
-}
-function puanhsp(){
-   
-   let a= 100-((degismeyenhak-hak-1)*(100/degismeyenhak));
-   return a;
-}
+    
