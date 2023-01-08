3a4e7cad6c01f951e6a0763f9c50a0add7249465
erentaskiran <101654590+erentaskiran@users.noreply.github.com>
2023-01-08 21:38:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,32 @@ let rndsayi=0;
 let tahmin=0;
 
 function oyun(){
-    rndsayi= Math.floor( ((Math.random()*10)+1));
+    alert("sayı tahmin oyununa hoş geldiniz");
+    hak=prompt("Kaç hakkınız olsun");
+    max=prompt("en büyük sayı");
+    min=prompt("en küçük sayı");
+
+    
+    rndsayi= Math.floor( ((Math.random()*max)+1));
+    while(rndsayi<min){
+        rndsayi= Math.floor( ((Math.random()*max)+1));
+    }
+
     do {
-        tahmin=  prompt("1 ile 10 arasında bir sayı tahmin edin");
-    if(tahmin==rndsayi){
+        tahmin=  prompt("1 ile 10 arasında bir sayı tahmin edin. hak sayınız:"+hak);
+        hak--;
+        if(hak==0){
+            alert("Hakkınız bitti");
+            break;
+        }
+        
+        if(tahmin==rndsayi){
         alert("kazandın");
-    }
-    else{
+
+        }
+        else{
         alert("yanlış");
-    }
+        }
     } while (tahmin!=rndsayi);
 }
     
